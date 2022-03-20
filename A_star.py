772b5b0fabd478b242b5f0c3a067e88ca88b6e4a
Gaurav <gauraut@umd.edu>
2022-03-20 15:22:10 -0400

    Added obstacle avoidance

--- A_star.py
+++ A_star.py
@@ -21,10 +21,26 @@ class Robot:
 
 
 	def move_possible(self, move, graph):
-		pass
+		mask = np.zeros(graph.shape)
+		shape_y, shape_x = graph.shape
+		if move[0] < 0 and move[0] >= shape_x and move[1] < 0 and move[1] >= shape_y:
+			return False
+		for i in range(shape_y):
+			for j in range(shape_x): # can optimize this
+				ec = (self.rd + self.cl)**2 - (j-self.pos[1])**2 - (i-self.pos[0])**2
+				if ec >= 0:
+					mask[i,j] = 1
+		import pdb; pdb.set_trace()
+		result = np.bitwise_and(graph.astype(np.uint8), mask.astype(np.uint8))
+		if result.any() == 1:
+			return False
+		else:
+			return True
+
+			
 
 	def action(self, storage, opn, visited, graph):
-		action_set = self.orn+[-np.pi/3, -np.pi/6, 0, np.pi/6, np.pi/3]
+		action_set = self.orn+np.array([-np.pi/3, -np.pi/6, 0, np.pi/6, np.pi/3])
 		for i in action_set:
 			new_x = round(self.step*np.cos(i))+self.pos[1]
 			new_y = round(self.step*np.sin(i))+self.pos[0]
@@ -35,14 +51,18 @@ class Robot:
 				if not exist(move, visited):
 					cost = self.cost + step
 					storage[len(storage)].append([len(storage), move, cost, self.parent])
+					visited.append(move)
+					opn.append([len(storage), move, cost, self.parent])
 				else:
-					location = find(move, visited)
+					location, location2 = find(move, visited, opn)
 					cost = self.cost + step
 					if storage[location][2] > cost:
 						storage[location][2] = cost
 						storage[location][3] = self.parent
+						opn[location2][2] = cost
+						opn[location2][3] = self.parent
 
-
+		return storage, opn, visited
 
 	def perform_action(self, move):
 		pass
@@ -60,6 +80,7 @@ def check(coords1, coords2):
 		return False
 
 def get_newnode(opn, goal):
+	# import pdb; pdb.set_trace()
 	ar_opn = np.array(opn)[:,1]
 	ar_opn = np.array([np.array(k) for k in ar_opn])
 	dist = np.sum((ar_opn[:2]-goal[:2])**2, axis=1)
@@ -68,12 +89,11 @@ def get_newnode(opn, goal):
 	return curr_node, opn
 
 def a_star(goal, robot):
-	storage = {}
 	graph = create_graph()
-	import pdb; pdb.set_trace()
-	closed = np.zeros((500,800,5))
-	storage[0].append([0, robot.pos, 0, 0]) # [num, coords, cost, parent]
-	closed[]
+	# import pdb; pdb.set_trace()
+	closed = []
+	storage = {0 : [0, robot.pos, 0, 0]} # [num, coords, cost, parent]
+	closed.append(robot.pos)
 	opn = []
 
 	while not check(robot.pos, goal):
